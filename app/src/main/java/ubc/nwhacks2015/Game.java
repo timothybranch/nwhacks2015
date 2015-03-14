a081b1a4997af126b0ee300b3362cba303073914
Tim Branch <timothy.branch@alumni.ubc.ca>
2015-03-14 11:30:50 -0700

    Added Game and Player classes.

---
+++ app/src/main/java/ubc/nwhacks2015/Game.java
@@ -0,0 +1,10 @@
+package ubc.nwhacks2015;
+
+/*
+ * This is the Game class, which is created on the hosts' app, and coordinates the game.
+ * Tentative.
+ */
+
+public class Game {
+    //Nothing is here yet.
+}
