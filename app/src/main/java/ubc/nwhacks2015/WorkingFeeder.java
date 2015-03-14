d82a2d5ed2dea8906d5d85ef898d25fc2b24e185
RZ404 <rzhang404@gmail.com>
2015-03-14 11:38:54 -0700

    Working

---
+++ app/src/main/java/ubc/nwhacks2015/WorkingFeeder.java
@@ -0,0 +1,13 @@
+package ubc.nwhacks2015;
+
+import
+/**
+ * Created by RayZ on 14/03/2015.
+ */
+
+//attempting to feed a line to a client
+public class WorkingFeeder {
+    cardID=Math.random
+    //if card is not destroyed, then accept it
+    if card
+}
