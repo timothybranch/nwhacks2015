a4ca3ad2098a625bc621fa33772121d3461b4efc
PotatoTank <joeyli96@hotmail.com>
2015-03-14 09:47:04 -0700

    Joey test.

--- app/src/androidTest/java/ubc/nwhacks2015/Test.java
+++ app/src/androidTest/java/ubc/nwhacks2015/Test.java
@@ -2,6 +2,6 @@ package ubc.nwhacks2015;
 
 public class Test {
     public static void main(String [] args){
-        System.out.print("Hi World!");
+        System.out.print("Hey World!");
     }
 }
