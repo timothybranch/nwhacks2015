ef3895301b658caf2b1931b8862fdd43eaaf27a2
Tim Branch <timothy.branch@alumni.ubc.ca>
2015-03-14 01:15:13 -0700

    Testing my edit.

--- app/src/androidTest/java/ubc/nwhacks2015/Test.java
+++ app/src/androidTest/java/ubc/nwhacks2015/Test.java
@@ -2,6 +2,6 @@ package ubc.nwhacks2015;
 
 public class Test {
     public static void main(String [] args){
-        System.out.print("Hello World!");
+        System.out.print("Goodbye World!");
     }
 }
