c902c76e442282f795bdd87fa1c2a6ce5a661a7b
Tim Branch <timothy.branch@alumni.ubc.ca>
2015-03-14 17:04:15 -0700

    Initialized Firebase library in the Activity.

--- app/src/main/java/ubc/nwhacks2015/CardCzar.java
+++ app/src/main/java/ubc/nwhacks2015/CardCzar.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.client.Firebase;
 
 public class CardCzar extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
         setContentView(R.layout.activity_card_czar);
     }
 
